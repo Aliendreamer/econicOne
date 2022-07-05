c355dfb51a528a63f7c3a60bbc0d5de0ad3a148b
Teo <teodor.g.stefanov@a1.bg>
2022-07-05 23:42:28 +0300

    initiated project and initial setup started

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		"eslint:recommended",
+		"plugin:react/jsx-runtime"
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: [
+		'react',
+	],
+	rules: {
+	},
+};
