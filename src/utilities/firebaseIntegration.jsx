0931c11b44f792d6e37b4204cc5408c8fe578f55
Teo <teodor.g.stefanov@a1.bg>
2022-07-06 00:00:51 +0300

    working on auth with firebase

--- src/utilities/firebaseIntegration.jsx
+++ src/utilities/firebaseIntegration.jsx
@@ -1,44 +1,13 @@
 import { initializeApp } from "firebase/app";
 import firebaseConfig from "../firebaseConfig.json";
-import {getAuth,signInWithEmailAndPassword,createUserWithEmailAndPassword,signOut} from "firebase/auth";
-import {getFirestore,collection,addDoc} from "firebase/firestore";
+import {getAuth,signOut} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-const logInWithEmailAndPassword = async (email, password) => {
-	try {
-		await signInWithEmailAndPassword(auth, email, password);
-	} catch (err) {
-		console.error(err);
-		alert(err.message);
-	}
-  };
-
-const registerWithEmailAndPassword = async (name, email, password) => {
-	try {
-		const res = await createUserWithEmailAndPassword(auth, email, password);
-		const user = res.user;
-		await addDoc(collection(db, "users"), {
-		uid: user.uid,
-		name,
-		authProvider: "local",
-		email,
-	});
-	} catch (err) {
-		console.error(err);
-		alert(err.message);
-	}
-  };
-
-  const logout = () => {
+const logout = () => {
 	signOut(auth);
-  };
+};
 
-  export {
-	auth,
-	db,
-	logInWithEmailAndPassword,
-	registerWithEmailAndPassword,
-	logout
-  };
+export {auth,db,logout};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /* eslint-disable no-undef */
 module.exports = {
 	content: ["./src/**/*.{js,jsx}"],
+	mode: "jit",
+	variants: {
+		display: ["children", "default", "children-first", "children-last", "children-odd", "children-even", "children-not-first", "children-not-last", "children-hover", "hover", "children-focus", "focus", "children-focus-within", "focus-within", "children-active", "active", "children-visited", "visited", "children-disabled", "disabled", "responsive"],
+		margin: ["responsive", "hover", "first", "last"]
+	},
+	prefix: "tw-",
+	important: true,
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require("@tailwindcss/forms"),
+		require("@tailwindcss/typography"),
+		require("tailwind-children"),
+		require("@tailwindcss/aspect-ratio")
+	],
 }
